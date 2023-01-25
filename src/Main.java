cbee58633b96a0d84674545e49c450be786d3a3d
TIME <opout777@yandex.ru>
2023-01-25 14:09:20 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    public static void task1() {
+        int[] arr = generateRandomArray();
+        int summ = 0;
+        for (int i = 0; i < arr.length; i++) {
+            summ += arr[i];
+            if (i == arr.length - 1) {
+                System.out.println("«Сумма трат за месяц составила "+ summ + " рублей».");
+            }
+        }
     }
 }
